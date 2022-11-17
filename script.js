cfe8097cc30210d410c57447d4346677edfd1dec
Wergiley <werslley.robson@gmail.com>
2022-11-17 20:21:01 -0300

    att

---
+++ script.js
@@ -0,0 +1,13 @@
+function imageList() {
+    const memeObject = {"nome": "chapolin", "path": "imagens/logo.jpeg",},
+                        {"nome": "xxxxx", "path": "imagens/chloe.jpeg"}
+    }
+}
+
+function galeria() {
+
+}
+
+function main() {
+
+}
