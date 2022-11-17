b43421e379b684f963123ab6d34afd97fc2fa46b
Wergiley <werslley.robson@gmail.com>
2022-11-17 20:25:47 -0300

    att

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 function imageList() {
-    const memeObject = {"nome": "chapolin", "path": "imagens/logo.jpeg",},
-                        {"nome": "xxxxx", "path": "imagens/chloe.jpeg"}
-    }
+    
 }
 
 function galeria() {
